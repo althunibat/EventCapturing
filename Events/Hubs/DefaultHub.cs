78ac70c1a5be534d5a5360f2795c6953c8964ea9
Hamza Althunibat <Althunibat@outlook.com>
2018-11-04 21:22:16 +0400

    initial commit

---
+++ Events/Hubs/DefaultHub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using EventStore.ClientAPI;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace Events.Hubs
+{
+    public class DefaultHub:Hub
+    {
+        private readonly IEventStoreConnection _connection;
+
+        public DefaultHub(IEventStoreConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task SendMessage(Message message)
+        {
+            await _connection.AppendToStreamAsync("web-stream", ExpectedVersion.Any,
+                new EventData(NewId.NextGuid(), "web-event", true,
+                    Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message)),
+                    Encoding.UTF8.GetBytes("signalr-generated")));
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
---
+++ Events/Hubs/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Events.Hubs
+{
+    public class Message
+    {
+        public string Event { get; set; }
+        public string Application { get; set; }
+        public string Area { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public override string ToString()
+        {
+            return $"{TimeStamp}:{Application}.{Area}-{Event}";
+        }
+    }
+}
